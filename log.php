b785f166994c461945685e8b6ade706f5ef0e518
P7r4nh4 <139864256+P7r4nh4@users.noreply.github.com>
2023-07-18 10:57:06 +0000

    Update log.php

--- log.php
+++ log.php
@@ -1,17 +1,5 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-
-    $log = "log.txt";
-    $data = "Ad: $name\nE-posta: $email\nMesaj: $message\n\n";
-
-    file_put_contents($log, $data, FILE_APPEND | LOCK_EX);
-
-    echo "Post successfull.";
-} else {
-    header("HTTP/1.1 405 Method Not Allowed");
-    echo "405 Method Not Allowed";
+if(isset($_POST['name'])){
+    echo 1;
 }
 ?>
